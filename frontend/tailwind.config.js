78c838e8cb59d5e23e377f7961ab09cad95ddd00
AlfinAkash <alfinakash2001@gmail.com>
2025-05-30 21:48:03 +0530

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,28 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
-
-	daisyui: {
-		themes: [
-		  "light",
-		  {
-			black: {
-			  ...require("daisyui/src/colors/themes")["[data-theme=black]"],
-			  primary: "rgb(29, 155, 240)",
-			  secondary: "rgb(24, 24, 24)",
-			},
-		  },
-		],
-	  }
-	  ,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          "primary": "rgb(29, 155, 240)",
+          "secondary": "rgb(24, 24, 24)",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#1a1a1a",
+          "info": "#2094f3",
+          "success": "#009485",
+          "warning": "#ff9900",
+          "error": "#ff5724",
+        },
+      },
+    ],
+  },
 };
